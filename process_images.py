c4a5baab44479330ab3b4830a6eddd3cb863ef7d
Vojtěch Molek <vojtechmolek@gmail.com>
2019-09-05 16:04:34 +0200

    Adapted to our dataset
    Adapted to our dataset of numbers. Minor changes to decapitated syntax of PyTorch. New classes for processing images and testing our model.

--- process_images.py
+++ process_images.py
@@ -5,34 +5,59 @@ import cv2
 from collections import defaultdict
 
 
+# using opencv2 for loading images in grayscale. Can be substituted to any other loading method that keeps format
+# [image height, image width, channels]
 def imread(path):
-    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
 
 
-def save_imgs_to_ndy(path, file_name, file_path):
-    tx_file_paths = glob.glob(path)
-    # get data shape from first image [img_count, img_w, img_h, channels]
-    data_shape = (len(tx_file_paths),) + imread(tx_file_paths[0]).shape
-    tx = np.zeros(data_shape)
+def save_imgs_to_ndy(data_dir_path, dataset_part, binary_file_dir_path):
+    """
+    saves images into numpy binary file. For consistency with original repository purposes. Keeping Omniglot formatting,
+    as implemented in original repository - batcher.py. Our classes are load one after another, from class digit 0 to
+    class digit 9.
+    :param data_dir_path: path to folder with images to be converted
+    :param dataset_part: test or train, to keep them separated
+    :param binary_file_dir_path: path to dir
+    """
+    # get all images in directory
+    data_paths = glob.glob(os.path.join(data_dir_path, '*.png'))
+    # get data shape from first image [img_count, img_h, img_w, channels]
+    data_shape = (len(data_paths),) + imread(data_paths[0]).shape
+    data_array = np.zeros(data_shape, dtype='uint8')
+
+    for datum_index in range(len(data_paths)):
+        # read all images and save them in data array
+        data_array[datum_index] = imread(data_paths[datum_index])
+    # save data array to binary numpy file
+    np.save(os.path.join(binary_file_dir_path, dataset_part + '_data.npy'), data_array, allow_pickle=False)
 
     labels = defaultdict(int)
-    for tx_file_path_index in range(len(tx_file_paths)):
-        label = tx_file_paths[tx_file_path_index].split(os.path.sep)[-1].split('-')[0]
+    for datum_index in range(len(data_paths)):
+        # split path to image by path separator and then split image name by '-'. First substring contains label.
+        label = data_paths[datum_index].split(os.path.sep)[-1].split('-')[0]
+        # increase given label counter
         labels[label] += 1
 
-    for tx_file_path_index in range(len(tx_file_paths)):
-        tx[tx_file_path_index] = imread(tx_file_paths[tx_file_path_index])
-
-    ty = np.zeros([len(labels.keys())])
+    label_counts_array = np.zeros([len(labels.keys())], dtype='int32')
+    # write dictionary to label_counts_array
     for key in labels.keys():
-        ty[int(key)] = labels[key]
+        label_counts_array[int(key)] = labels[key]
 
-    np.save(os.path.join(file_path, file_name + '_data.npy'), tx, allow_pickle=False)
-    np.save(os.path.join(file_path, file_name + '_counts.npy'), ty, allow_pickle=False)
+    # save label counts to binary numpy file
+    np.save(os.path.join(binary_file_dir_path, dataset_part + '_sizes.npy'), label_counts_array, allow_pickle=False)
+    # compute start of each class within array
+    class_starts = np.cumsum(label_counts_array)
+    class_starts = np.roll(class_starts, 1)
+    class_starts[0] = 0
+    # and save it to the binary numpy file
+    np.save(os.path.join(binary_file_dir_path, dataset_part + '_starts.npy'), label_counts_array, allow_pickle=False)
 
 
 if __name__ == '__main__':
-    path = os.path.join('data', 'letters_learn_es_pp', '*.png')
-    save_imgs_to_ndy(path, 'train.npy', 'data')
-    path = os.path.join('data', 'letters_recogn_es_pp', '*.png')
-    save_imgs_to_ndy(path, 'test.npy', 'data')
+    # process training images# compute start of each class within array
+    train_dataset_path = os.path.join('data', 'letters_learn_es_pp')
+    save_imgs_to_ndy(train_dataset_path, 'train', 'data')
+    # process training images# compute start of each class within array
+    test_dataset_path = os.path.join('data', 'letters_recogn_es_pp')
+    save_imgs_to_ndy(test_dataset_path, 'test', 'data')
--- train.py
+++ train.py
@@ -11,8 +11,9 @@ from models import ArcBinaryClassifier
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchSize', type=int, default=128, help='input batch size')
-parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the input image to ARC')
+parser.add_argument('--batchSize', type=int, default=1000, help='input batch size')
+parser.add_argument('--imageHeight', type=int, default=28, help='the height of the input image to ARC')
+parser.add_argument('--imageWidth', type=int, default=26, help='the width of the input image to ARC')
 parser.add_argument('--glimpseSize', type=int, default=8, help='the height / width of glimpse seen by ARC')
 parser.add_argument('--numStates', type=int, default=128, help='number of hidden states in ARC controller')
 parser.add_argument('--numGlimpses', type=int, default=6, help='the number glimpses of each image in pair seen by ARC')
@@ -25,7 +26,7 @@ parser.add_argument('--load', default=None, help='the model to load from. Start 
 
 def get_pct_accuracy(pred: Variable, target) -> int:
     hard_pred = (pred > 0.5).int()
-    correct = (hard_pred == target).sum().data[0]
+    correct = (hard_pred == target).sum().item()
     accuracy = float(correct) / target.size()[0]
     accuracy = int(accuracy * 100)
     return accuracy
@@ -71,10 +72,10 @@ def train():
     optimizer = torch.optim.Adam(params=discriminator.parameters(), lr=opt.lr)
 
     # load the dataset in memory.
-    loader = Batcher(batch_size=opt.batchSize, image_size=opt.imageSize)
+    loader = Batcher(batch_size=opt.batchSize)
 
     # ready to train ...
-    best_validation_loss = None
+    best_test_loss = None
     saving_threshold = 1.02
     last_saved = datetime.utcnow()
     save_every = timedelta(minutes=10)
@@ -87,34 +88,35 @@ def train():
         pred = discriminator(X)
         loss = bce(pred, Y.float())
 
-        if i % 10 == 0:
+        if i % 100 == 0:
 
-            # validate your model
-            X_val, Y_val = loader.fetch_batch("val")
-            pred_val = discriminator(X_val)
-            loss_val = bce(pred_val, Y_val.float())
+            # Since we do not have validation set, we use test set to see how is our model performing. We did not modify
+            # any hyperparameters on basis of test accuracy. This is purely informational.
+            X_test, Y_test = loader.fetch_batch('test')
+            pred_val = discriminator(X_test)
+            loss_val = bce(pred_val, Y_test.float())
 
-            training_loss = loss.data[0]
-            validation_loss = loss_val.data[0]
+            training_loss = loss.item()
+            test_loss = loss_val.item()
 
-            print("Iteration: {} \t Train: Acc={}%, Loss={} \t\t Validation: Acc={}%, Loss={}".format(
-                i, get_pct_accuracy(pred, Y), training_loss, get_pct_accuracy(pred_val, Y_val), validation_loss
+            print("Iteration: {} \t Train: Acc={}%, Loss={} \t\t Test: Acc={}%, Loss={}".format(
+                i, get_pct_accuracy(pred, Y), training_loss, get_pct_accuracy(pred_val, Y_test), test_loss
             ))
 
-            if best_validation_loss is None:
-                best_validation_loss = validation_loss
+            if best_test_loss is None:
+                best_test_loss = test_loss
 
-            if best_validation_loss > (saving_threshold * validation_loss):
-                print("Significantly improved validation loss from {} --> {}. Saving...".format(
-                    best_validation_loss, validation_loss
+            if best_test_loss > (saving_threshold * test_loss):
+                print("Significantly improved test loss from {} --> {}. Saving...".format(
+                    best_test_loss, test_loss
                 ))
-                discriminator.save_to_file(os.path.join(models_path, str(validation_loss)))
-                best_validation_loss = validation_loss
+                discriminator.save_to_file(os.path.join(models_path, str(test_loss)))
+                best_test_loss = test_loss
                 last_saved = datetime.utcnow()
 
             if last_saved + save_every < datetime.utcnow():
                 print("It's been too long since we last saved the model. Saving...")
-                discriminator.save_to_file(os.path.join(models_path, str(validation_loss)))
+                discriminator.save_to_file(os.path.join(models_path, str(test_loss)))
                 last_saved = datetime.utcnow()
 
         optimizer.zero_grad()
